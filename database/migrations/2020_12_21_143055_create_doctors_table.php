56e0ec0a26d1a37c2426e7d4d976516487e776a4
clement-ximenes <69210697+clement-ximenes@users.noreply.github.com>
2020-12-21 16:48:48 +0100

    wip

---
+++ database/migrations/2020_12_21_143055_create_doctors_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDoctorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctor', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstName', 255);
+            $table->string('lastName', 255);
+            $table->text('postalAddress');
+            $table->integer('postCode');
+            $table->float('latitude', 15, 8);
+            $table->float('longitude', 15, 8);
+            $table->text('description');
+            $table->string('picture');
+            $table->integer('price');
+            $table->string('email')->unique();
+            $table->string('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctor');
+    }
+}
---
+++ database/migrations/2020_12_21_143149_create_schedules_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedule', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('doctorId')->references('id')->on('doctor');
+            $table->foreign('patientId')->references('id')->on('patient')->nullable();
+            $table->date('start');
+            $table->date('end');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
